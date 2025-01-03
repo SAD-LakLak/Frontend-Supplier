859e7991a96a44b146a934dc4f451f9e4b16a637
mahyazz <mahya.zarrabi@gmail.com>
2025-01-03 22:12:12 +0330

    fix: update landing

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -12,12 +12,12 @@ const settings = {
     slidesToScroll: 1,
     infinite: true,
     autoplay: true,
-    autoplaySpeed: 5000,
+    autoplaySpeed: 4000,
 };
 
 function Home({children}: { children: React.ReactNode }) {
     return (
-        <div className="flex flex-col px-16 py-4 gap-4 bg-primaryLight h-screen justify-between relative">
+        <div className="flex flex-col px-16 py-4 gap-4 bg-primaryLight h-auto justify-between relative">
             <Header/>
 
             {/* Banners */}
@@ -34,10 +34,34 @@ function Home({children}: { children: React.ReactNode }) {
                 </div>
             </Slider>
 
-            {/* محتوا وسط صفحه */}
-            <div className="absolute inset-0 mt-24 mb-20 mx-16 flex justify-center items-center z-10">
-                {children}
+            {/* Branding 101 */}
+            <div>
+                <img src="/images/home-page.png" alt="Home Background"/>
             </div>
+
+            {/* <div className="relative">
+                <img className="absolute top-0 left-0" src="/images/blob-bg.png" alt="Home Background"/>
+                <div className="absolute top-0 left-0">
+                    <div className="flex flex-col items-end">
+                        <p>
+                            ماجرای لک‌لک
+                        </p>
+                        <p>
+                            خانواده‌ی لک‌لک دور هم جمع شدند تا خوش‌آمد‌گویی به اعضای جدید خانواد‌ه‌های دیگر را ‌راحت‌تر و دلنشین‌تر کنند.
+                        </p>
+                        <p>
+                            اگر کسب‌وکار شما محصولات یا خدماتی ارائه می‌دهد که والدین چشم به راه، مادرانی که تازه زایمان کرده‌اند، یا نوزادان به آن احتیاج دارند، مشتاق پیوستن شما به خانواده‌ی کوچکمان هستیم.
+                        </p>
+                        <p>
+                            گروه لک‌لک محصولات و خدمات شما را در بسته‌های کاربردی و جذاب دسته‌بندی می‌کند تا هم دردسر خرید برای کاربران کم شود، و هم کسب‌وکار شما به مشتریان جدید معرفی شود.
+                        </p>
+                    </div>
+                </div>
+            </div> */}
+
+            {/* <div className="absolute inset-0 mt-24 mb-20 mx-16 flex justify-center items-center z-10">
+                {children}
+            </div> */}
 
             <Footer/>
         </div>
