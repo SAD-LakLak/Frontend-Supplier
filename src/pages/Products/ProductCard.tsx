fb80842bb482f5831c190d6e879e88d72eee7a5d
Moeein Aali <moeeeinaali@gmail.com>
2025-01-26 23:29:22 +0330

    Fix persian numbers

--- src/pages/Products/ProductCard.tsx
+++ src/pages/Products/ProductCard.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import {Product} from '../../types/Product';
-import {replacePersianNumbers} from "../../utils/replacePersianNumbers.ts";
+import {replaceEnglishDigits} from "../../utils/replacePersianNumbers.ts";
 import {Button} from "@material-tailwind/react";
+import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
+import BorderColorOutlinedIcon from '@mui/icons-material/BorderColorOutlined';
 
 interface ProductCardProps {
     product: Product;
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({product}) => {
-    console.log(product)
     return (
-        <div className={"rounded-xl debug h-12 flex flex-row-reverse justify-between items-center px-4"}>
+        // TODO: Add Edit/Delete Post Page
+        <div
+            className={"rounded-xl h-12 flex flex-row-reverse justify-between items-center px-4 py-6 border-2"}>
             <p className={"font-IRANSansXMedium"}>{product.name}</p>
-            <p className={"font-IRANSansXDemiBold"}
-               dir={"rtl"}>{replacePersianNumbers(String(product.price)) + " تومان "}</p>
+            <p className={"font-IRANSansXDemiBold w-1/5 text-left"}
+               dir={"rtl"}>{replaceEnglishDigits(String(product.price)) + " تومان "}</p>
+            <p className={"font-IRANSansXDemiBold w-1/6 justify-end text-left"}
+               dir={"rtl"}>{replaceEnglishDigits(String(product.stock)) + " عدد "}</p>
             <Button disabled={true}
                     className="rounded-full w-fit bg-accent opacity-70  font-IRANSansXDemiBold">غیرفعال</Button>
+            <div className={'flex items-center gap-2 justify-between'}>
+                <DeleteForeverOutlinedIcon className={"text-accent hover:cursor-pointer"} fontSize={"large"}/>
+                <BorderColorOutlinedIcon className={"text-primary hover:cursor-pointer"} fontSize={"medium"}/>
+            </div>
         </div>
     );
 };
--- src/utils/replacePersianNumbers.ts
+++ src/utils/replacePersianNumbers.ts
@@ -8,6 +8,20 @@ export const replacePersianNumbers = (input: string): string => {
     const regex = new RegExp(persianNumbers[i], "g");
     result = result.replace(regex, englishNumbers[i]);
   }
+  console.log(result)
+  return result;
+};
 
+export const replaceEnglishDigits = (input: string): string => {
+  const englishNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const persianNumbers = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
+
+  let result = input;
+
+  for (let i = 0; i < englishNumbers.length; i++) {
+    const regex = new RegExp(englishNumbers[i], "g");
+    result = result.replace(regex, persianNumbers[i]);
+  }
+  console.log(result);
   return result;
 };
