dba2777854c313c86da4528c18610e475d7391f2
mahyazz <mahya.zarrabi@gmail.com>
2025-02-08 03:34:52 +0330

    fix: update replacePersianNumbers input types

--- src/utils/replacePersianNumbers.ts
+++ src/utils/replacePersianNumbers.ts
@@ -1,8 +1,8 @@
-export const replacePersianNumbers = (input: string): string => {
+export const replacePersianNumbers = (input: string | number): string => {
   const persianNumbers = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
   const englishNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-  let result = input;
+  let result = typeof input === "number" ? input.toString() : input;
 
   for (let i = 0; i < persianNumbers.length; i++) {
     const regex = new RegExp(persianNumbers[i], "g");
